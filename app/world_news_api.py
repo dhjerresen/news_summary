e94ebc9652fde7514c448f21659bf9e6f0f1d1fc
Daniel Hjerresen <danielhjerresen@hotmail.com>
2026-04-07 14:25:51 +0200

    updated how key for news api is loaded

--- app/world_news_api.py
+++ app/world_news_api.py
@@ -8,13 +8,13 @@ from pathlib import Path
 import requests
 from dotenv import load_dotenv
 
-def fetch_news() -> dict:
-    env_path = Path(__file__).resolve().parent.parent / ".env"
-    load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
+def fetch_news() -> dict:
     api_key = os.getenv("WORLDNEWS_API_KEY")
+
     if not api_key:
-        raise ValueError("WORLDNEWS_API_KEY not found in .env")
+        raise ValueError("WORLDNEWS_API_KEY not found in environment variables")
 
     today = date.today().isoformat()
     url = "https://api.worldnewsapi.com/top-news"
